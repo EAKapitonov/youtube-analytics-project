9a090b2bec302e0545a13824599f9465772708c7
Egor <Kapitonov281087@mail.ru>
2023-03-11 19:11:09 +0900

    hw_2 NOT FULL

--- src/channel.py
+++ src/channel.py
@@ -12,14 +12,43 @@ api_key: str = os.getenv('youtube_api')
 # создать специальный объект для работы с API
 youtube = build('youtube', 'v3', developerKey=api_key)
 
+
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self._channel_id = channel_id
+        self.title = channel["items"][0]["snippet"]["title"]
+        self.description = channel["items"][0]["snippet"]["description"]
+        self.url = f"https://www.youtube.com/channel/{channel_id}"
+        self.subscriberCount = channel["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = channel["items"][0]["statistics"]["videoCount"]
+        self.viewCount = channel["items"][0]["statistics"]["viewCount"]
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    @property
+    def channel_id(self):
+        """ чтобы id нельзя было поменять из вне"""
+        return self._channel_id
+
+    @staticmethod
+    def get_service():
+        """ получить объект для работы с API вне класса"""
+        import src
+        return src.channel.youtube
+
+    @property
+    def to_json(self, file_):
+        """создаем файл 'file' c данными по каналу"""
+        with open(file_, 'w', encoding="utf-8") as f:
+            channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+            name = json.dumps(channel, indent=2, ensure_ascii=False)
+            f.write(name)
+
+
